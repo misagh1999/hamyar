dc4e62e6386240d2b3c67b9621a770c5f3a14c90
M.Misaghpour <misagh1999@gmail.com>
2026-06-05 12:29:01 +0330

    use BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Link, Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter, Link, Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CaseDetailPage } from './pages/CaseDetailPage';
 import { HomePage } from './pages/HomePage';
@@ -39,10 +39,10 @@ function Shell() {
 
 export function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <Shell />
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
